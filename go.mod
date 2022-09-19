b1f738a8460b8beba1b09746d1be7d0dda427fa6
cisnux-seed <fajrarisqulla@gmail.com>
2022-09-20 03:38:40 +0700

    add max to int validation

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/cisnux-seed/cloudy-validations
+module github.com/cisnux-seed/cloudy-validations/v2
 
 go 1.19
--- validations.go
+++ validations.go
@@ -19,12 +19,12 @@ func doValidations[T interface{}]() map[string]func(T, reflect.StructField, int)
 		reflect.Int.String(): func(object T, field reflect.StructField, i int) (isValidationPresent bool, isValidated bool) {
 			isValidated = true
 			isValidationPresent = false
-			if field.Tag.Get("required") == "true" {
-				isValidated = reflect.ValueOf(&object).Elem().Field(i).Int() != 0
-				isValidationPresent = true
-			}
 			if min, error := strconv.ParseInt(field.Tag.Get("min"), 10, 32); error == nil {
 				isValidated = reflect.ValueOf(&object).Elem().Field(i).Int() >= min
+				isValidationPresent = true
+			}
+			if max, error := strconv.ParseInt(field.Tag.Get("max"), 10, 32); error == nil {
+				isValidated = reflect.ValueOf(&object).Elem().Field(i).Int() <= max
 				isValidationPresent = true
 			}
 			return
